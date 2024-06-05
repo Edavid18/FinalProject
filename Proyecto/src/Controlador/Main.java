2bd2b686dcf26b5375b2710da2da618a39d1b268
Andres Franco <milofrank777@gmail.com>
2024-06-05 17:51:42 -0500

    JavaFx
    Se creo el Archivo de Java Fx con todos sus controladores

---
+++ Proyecto/src/Controlador/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Controlador;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author milof
+ */
+
+public class Main extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Application.launch();
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    primaryStage.show();
+    }
+    
+}
