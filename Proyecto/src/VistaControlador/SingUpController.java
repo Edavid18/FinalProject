38ecac4bc5ba511dc63da56882eae5a0b8d8ab6a
Andres Franco <milofrank777@gmail.com>
2024-06-19 01:02:37 -0500

    Create Log in View Javafx
    Un avance de la vista del Log in

---
+++ Proyecto/src/VistaControlador/SingUpController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package VistaControlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author milof
+ */
+public class SingUpController implements Initializable {
+
+    @FXML
+    private TextField texto;
+    @FXML
+    private Label etiqueta;
+    @FXML
+    private Button boton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+   
+    
+}
