d17fef0fedcd7182cd2c852aaefe5d09612dc3c2
Andres Franco <milofrank777@gmail.com>
2024-06-19 20:58:21 -0500

    Sign Up and Log In
    Vista del Sign Up y Log In rediseñadas

--- Proyecto/src/VistaControlador/SignUpController.java
+++ Proyecto/src/VistaControlador/SignUpController.java
@@ -4,9 +4,17 @@
  */
 package VistaControlador;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,5 +30,26 @@ public class SignUpController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void Back(ActionEvent event) {
+        try{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vista/LogIn.fxml"));
+        Parent root = loader.load();
+        LogInController controlador = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+       // stage.initModality(Modality.APPLICATION_MODAL); sirve para no salir hasta terminar el programa
+        stage.setScene(scene);
+        
+        stage.show();
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+        
+        }
+        catch(IOException ex){
+           
+        }
+    }
     
 }
