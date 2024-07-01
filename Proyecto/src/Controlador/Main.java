5afe32c232c4f54ba0f258de891eaca4a2a733c0
Andres Franco <milofrank777@gmail.com>
2024-07-01 02:00:58 -0500

    Vista General Item
    Avance Significativo para las vista general de cada Ítem del catalogo

--- Proyecto/src/Controlador/Main.java
+++ Proyecto/src/Controlador/Main.java
@@ -4,7 +4,6 @@
  */
 package Controlador;
 
-import VistaControlador.VistaCompraController;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -13,7 +12,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -29,16 +27,11 @@ public class Main extends Application{
 
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("/Vista/VistaCompra.fxml"));
+            loader.setLocation(Main.class.getResource("/Vista/Catalogue.fxml"));
             Pane ventana = (Pane) loader.load();
 
             // Show the scene containing the root layout.
             Scene scene = new Scene(ventana);
-            Stage stage = new Stage();
-            VistaControlador.VistaCompraController ins = loader.getController();
-            ins.VistaGen("Caña", 1000);
-             
-             
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
@@ -48,27 +41,9 @@ public class Main extends Application{
 
     public static void main(String[] args) {
         launch(args);
-         //pedirValoresYImprimir();
     }
    
-   public static void pedirValoresYImprimir() {
-        // Array para almacenar los 5 valores
-        String[] valores = new String[5];
-        
-        // Bucle para pedir los 5 valores
-        for (int i = 0; i < 5; i++) {
-            valores[i] = JOptionPane.showInputDialog(null, "Introduce el valor " + (i + 1) + ":");
-        }
-        
-        // Imprimir los valores en la consola
-        System.out.println("Los valores introducidos son:");
-        for (String valor : valores) {
-            System.out.println(valor);
-        }
+   
 
     
-}
-   
-   
-   
 }
