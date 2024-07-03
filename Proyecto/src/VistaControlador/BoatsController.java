632c66c857571a0de6375547e7d0390549c566f2
Davllld <eliasdavidvidal24@gmail.com>
2024-07-03 17:55:44 -0500

    Added "boats" panel views.

---
+++ Proyecto/src/VistaControlador/BoatsController.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package VistaControlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author milof
+ */
+public class BoatsController implements Initializable {
+
+    @FXML
+    private AnchorPane rootpane;
+    @FXML
+    private ScrollPane RootPanel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void FS1800(MouseEvent event) {
+    }
+
+    @FXML
+    private void LWBay(MouseEvent event) {
+    }
+
+    @FXML
+    private void VW1670(MouseEvent event) {
+    }
+
+    @FXML
+    private void Roughneck2070(MouseEvent event) {
+    }
+
+    @FXML
+    private void lbrs270(MouseEvent event) {
+    }
+
+    @FXML
+    private void sf232(MouseEvent event) {
+    }
+
+    @FXML
+    private void rs270(MouseEvent event) {
+    }
+
+    @FXML
+    private void Mt1885(MouseEvent event) {
+    }
+
+    @FXML
+    private void lb115(MouseEvent event) {
+    }
+
+    @FXML
+    private void Stringer198(MouseEvent event) {
+    }
+
+    @FXML
+    private void Fishingmachine(MouseEvent event) {
+    }
+
+    @FXML
+    private void thevw1670(MouseEvent event) {
+    }
+
+    @FXML
+    private void lblegacy(MouseEvent event) {
+    }
+
+    @FXML
+    private void rougneck2070(MouseEvent event) {
+    }
+
+    @FXML
+    private void fishandsly(MouseEvent event) {
+    }
+
+    @FXML
+    private void sd224(MouseEvent event) {
+    }
+    
+}
